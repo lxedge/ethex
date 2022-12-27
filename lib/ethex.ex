fa11d1da673fc42176983cb15a69ac23097144b4
Li Xing <lxedge@sina.com>
2022-12-27 18:26:22 +0800

    init commit

---
+++ lib/ethex.ex
@@ -0,0 +1,18 @@
+defmodule Ethex do
+  @moduledoc """
+  Documentation for `Ethex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ethex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ethex_test.exs
@@ -0,0 +1,8 @@
+defmodule EthexTest do
+  use ExUnit.Case
+  doctest Ethex
+
+  test "greets the world" do
+    assert Ethex.hello() == :world
+  end
+end
