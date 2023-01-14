eff1794e36cf26809521bfe12266a5d8567ba33c
Li Xing <lxedge@sina.com>
2023-01-14 19:54:58 +0800

    change version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ethex.MixProject do
   def project do
     [
       app: :ethex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       description: "Ethereum cross chain json-rpc implement based on ex_abi",
       package: [
